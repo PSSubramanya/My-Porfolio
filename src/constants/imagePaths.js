94ee78e2a80ba5ffcdf45558612288e3c4fc8651
pssubramanyabhat <subbukarthikeya98@gmail.com>
2025-09-13 11:50:09 +0530

    Added 4 sections to the page

--- src/constants/imagePaths.js
+++ src/constants/imagePaths.js
@@ -5,6 +5,20 @@ const imagePath = {
   // Icons
   reactIcon: require("../assets/images/icons/reactIcon.png"),
   webDevelopmentIcon: require("../assets/images/icons/webDevelopmentIcon.png"),
+  callIcon: require("../assets/images/icons/callIcon.png"),
+  chatIcon: require("../assets/images/icons/chatIcon.png"),
+  mailIcon: require("../assets/images/icons/mailIcon.png"),
+  linkedInIcon: require("../assets/images/icons/linkedInIcon.png"),
+
+  // App Screenshots
+  screenshot1: require("../assets/images//screenshots/ss1.png"),
+  screenshot2: require("../assets/images//screenshots/ss2.png"),
+  screenshot3: require("../assets/images//screenshots/ss3.png"),
+  screenshot4: require("../assets/images//screenshots/ss4.png"),
+  screenshot5: require("../assets/images//screenshots/ss5.png"),
+  screenshot6: require("../assets/images//screenshots/ss6.png"),
+  screenshot7: require("../assets/images//screenshots/ss7.jpeg"),
+  screenshot8: require("../assets/images//screenshots/ss8.jpeg"),
 };
 
 export default imagePath;
--- src/pages/HomeScreen/HomeScreen.js
+++ src/pages/HomeScreen/HomeScreen.js
@@ -1,4 +1,5 @@
 import react from "react";
+import { Link } from "react-scroll";
 import "./HomeScreen.css";
 import imagePath from "../../constants/imagePaths";
 
@@ -6,8 +7,8 @@ const HomeScreen = () => {
   return (
     <div>
       <div className="row-style header-spacing-style" id="header-section">
-        <a
-          href="#home"
+        <Link
+          to="home"
           className="head-text"
           smooth={true}
           duration={500}
@@ -15,9 +16,9 @@ const HomeScreen = () => {
           activeClass="active"
         >
           Home
-        </a>
-        <a
-          href="#about"
+        </Link>
+        <Link
+          to="about"
           className="head-text"
           smooth={true}
           duration={500}
@@ -25,9 +26,9 @@ const HomeScreen = () => {
           activeClass="active"
         >
           About
-        </a>
-        <a
-          href="#projects"
+        </Link>
+        <Link
+          to="projects"
           className="head-text"
           smooth={true}
           duration={500}
@@ -35,9 +36,9 @@ const HomeScreen = () => {
           activeClass="active"
         >
           Projects
-        </a>
-        <a
-          href="#contact"
+        </Link>
+        <Link
+          to="contact"
           className="head-text"
           smooth={true}
           duration={500}
@@ -45,7 +46,7 @@ const HomeScreen = () => {
           activeClass="active"
         >
           Contact
-        </a>
+        </Link>
       </div>
 
       <section id="home">
@@ -129,7 +130,7 @@ const HomeScreen = () => {
           <div className="row-style tech-stack-container">
             <img
               src={imagePath?.reactIcon}
-              className="tech-stack-icon"
+              className="portfolio-icons"
               alt="mobile-app-development"
             />
             <span className="tech-stack-text">Mobile App Development</span>
@@ -138,7 +139,7 @@ const HomeScreen = () => {
           <div className="row-style tech-stack-container">
             <img
               src={imagePath?.webDevelopmentIcon}
-              className="tech-stack-icon"
+              className="portfolio-icons"
               alt="web-app-development"
             />
             <span className="tech-stack-text">Website Development</span>
@@ -149,7 +150,7 @@ const HomeScreen = () => {
             <div className="row-style tech-stack-container">
               <img
                 src={imagePath?.reactIcon}
-                className="tech-stack-icon"
+                className="portfolio-icons"
                 alt="mobile-app-development"
               />
               <span className="tech-stack-text">Mobile App Development</span>
@@ -158,14 +159,14 @@ const HomeScreen = () => {
             <div className="row-style tech-stack-container">
               <img
                 src={imagePath?.webDevelopmentIcon}
-                className="tech-stack-icon"
+                className="portfolio-icons"
                 alt="web-app-development"
               />
               <span className="tech-stack-text">Website Development</span>
             </div>
           </div>
           <div id="about-me">
-            <span id="about-me-text">About me</span>
+            <span className="section-header-text">About me</span>
             <span id="about-me-description">
               I have my bachelor degree in Computer Science Engineering. I
               started with Front end Mobile application development using React
@@ -193,6 +194,184 @@ const HomeScreen = () => {
                   Total years of experience
                 </span>
               </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="projects">
+        <span className="section-header-text" id="section-header">
+          PROJECTS
+        </span>
+        <div id="projects-section">
+          <div className="row-style project-display">
+            <div className="row-style project-screenshots screenhots-leftpad">
+              <img
+                src={imagePath?.screenshot1}
+                className="screenshot-styling"
+                alt="ss1"
+              />
+              <img
+                src={imagePath?.screenshot2}
+                className="screenshot-styling screenshot-spacing"
+                alt="ss2"
+              />
+            </div>
+            <div className="column-style project-details">
+              <span className="project-text">AniVault</span>
+              <span className="project-description">
+                An anime and manga hub where users can explore detailed
+                information on shows and series. It also acts as a personal
+                vault, allowing users to track, manage, and organize their anime
+                watchlists and completed titles.
+              </span>
+            </div>
+          </div>
+          <div className="divider-style" />
+          <div className="row-style project-display">
+            <div className="column-style project-details media-queried-details-one">
+              <span className="project-text">FurniBazar</span>
+              <span className="project-description">
+                An e-commerce style mobile application focused exclusively on
+                furniture and table-based equipment. Similar to Amazon but
+                tailored to home and office furniture, enabling users to browse,
+                compare, and purchase with ease.
+              </span>
+            </div>
+            <div className="row-style project-screenshots right-style screenhots-rightpad">
+              <img
+                src={imagePath?.screenshot3}
+                className="screenshot-styling"
+                alt="ss3"
+              />
+              <img
+                src={imagePath?.screenshot4}
+                className="screenshot-styling screenshot-spacing"
+                alt="ss4"
+              />
+            </div>
+            <div className="column-style project-details media-queried-details-two">
+              <span className="project-text">FurniBazar</span>
+              <span className="project-description">
+                An e-commerce style mobile application focused exclusively on
+                furniture and table-based equipment. Similar to Amazon but
+                tailored to home and office furniture, enabling users to browse,
+                compare, and purchase with ease.
+              </span>
+            </div>
+          </div>
+          <div className="divider-style" />
+          <div className="row-style project-display">
+            <div className="row-style project-screenshots screenhots-leftpad">
+              <img
+                src={imagePath?.screenshot5}
+                className="screenshot-styling"
+                alt="ss5"
+              />
+              <img
+                src={imagePath?.screenshot6}
+                className="screenshot-styling screenshot-spacing"
+                alt="ss6"
+              />
+            </div>
+            <div className="column-style project-details">
+              <span className="project-text">Poke Guide</span>
+              <span className="project-description">
+                A React Native mobile app that serves as a PokémonGo guide,
+                providing a complete Pokédex, event updates, and gameplay tips.
+                Helps players track Pokémon details, stay updated on in-game
+                events, and improve their overall experience
+              </span>
+            </div>
+          </div>
+          <div className="divider-style" />
+          <div className="row-style project-display">
+            <div className="column-style project-details media-queried-details-one">
+              <span className="project-text">KitchenGRAM</span>
+              <span className="project-description">
+                A social media-inspired app like Instagram but dedicated to food
+                lovers. Users can share food photos, discover new recipes,
+                follow chefs, and even order ingredients or meals directly from
+                creators.
+              </span>
+            </div>
+            <div className="row-style project-screenshots right-style screenhots-rightpad">
+              <img
+                src={imagePath?.screenshot7}
+                className="screenshot-styling"
+                alt="ss7"
+              />
+              <img
+                src={imagePath?.screenshot8}
+                className="screenshot-styling screenshot-spacing"
+                alt="ss8"
+              />
+            </div>
+            <div className="column-style project-details media-queried-details-two">
+              <span className="project-text">KitchenGRAM</span>
+              <span className="project-description">
+                A social media-inspired app like Instagram but dedicated to food
+                lovers. Users can share food photos, discover new recipes,
+                follow chefs, and even order ingredients or meals directly from
+                creators.
+              </span>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className="divider-style" />
+
+      <section id="contact">
+        <span className="section-header-text" id="contacts-section">
+          CONTACT US
+        </span>
+        <div className="row-style contact-medium-spacing-style">
+          <div className="contacting-box">
+            <img
+              src={imagePath?.mailIcon}
+              className="portfolio-icons"
+              alt="emil-us"
+            />
+            <span className="contacting-medium-heder-text">Email us</span>
+            <span className="contacting-medium-description-text">
+              From your email
+            </span>
+
+            <div className="small-divider">
+              <span className="contacting-medium-text">
+                subbukarthikeya98@gmail.com
+              </span>
+            </div>
+          </div>
+
+          <div className="contacting-box">
+            <img
+              src={imagePath?.callIcon}
+              className="portfolio-icons"
+              alt="call-or-text"
+            />
+            <span className="contacting-medium-heder-text">Call or text</span>
+            <span className="contacting-medium-description-text">
+              Mobile App Development
+            </span>
+            <div className="small-divider">
+              <span className="contacting-medium-text">+91 7204958072</span>
+            </div>
+          </div>
+
+          <div className="contacting-box">
+            <img
+              src={imagePath?.chatIcon}
+              className="portfolio-icons"
+              alt="linkedin"
+            />
+            <span className="contacting-medium-heder-text">Chat with us</span>
+            <span className="contacting-medium-description-text">
+              Chat with us on LinkedIn
+            </span>
+            <div className="small-divider">
+              <span className="contacting-medium-text">Go to LinkedIn</span>
             </div>
           </div>
         </div>
